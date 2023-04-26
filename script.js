0df72c70c8e2acc45ab2e73adbba1d979f3ec013
Shadi38 <113238379+Shadi38@users.noreply.github.com>
2023-04-26 23:40:20 +0100

    adding search bar

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-//You can edit ALL of the code here
+//variables:
+const rootElem = document.getElementById("root");
+const lable = document.getElementById("lable");
+
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -27,3 +31,39 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup();
+
+
+
+// acces to input's value
+const input = document.querySelector("#search-bar");
+input.addEventListener("input", function () {
+  let result = "";
+  const inputValue = input.value;
+  const allEpisodes = getAllEpisodes();
+  
+  let selectedEpisodes = allEpisodes.filter((episode) => {
+    if (
+      episode.name.includes(inputValue) ||
+      episode.summary.includes(inputValue)
+    ) {
+     return episode; 
+    }
+  });
+  
+rootElem.innerText = "";
+selectedEpisodes.forEach((selectEpisode)=>{
+
+result += `
+    <div style="background-color:white; width:250px; higth:300px;">
+    <h1 style="color:black;">${selectEpisode.name}</h1>
+    <img src="${selectEpisode.image.medium}"></img>
+    <div><p>${selectEpisode.summary}</p></div>
+    </div>
+    `;
+})
+ rootElem.innerHTML = result;
+lable.innerText = `Displaying  ${selectedEpisodes.length}/${allEpisodes.length}  episodes`;
+   
+});
+
+ 
