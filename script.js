a13615d7a00758864716f5d725e07b15648f5cde
Shadi38 <113238379+Shadi38@users.noreply.github.com>
2023-04-26 14:32:11 +0100

    finished episodes page

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
+   let output="";
+  episodeList.forEach((episode) => {
+    
+    output += `
+    <div style="background-color:white; width:250px; higth:300px;">
+    <h1 style="color:black;">${episode.name}</h1>
+    <img src="${episode.image.medium}"></img>
+    <div><p>${episode.summary}</p></div>
+    </div>
+    `;
+    
+  });
+
+
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = output;
+  let episodeNumber = document.createElement("div");
+  episodeNumber.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.appendChild(episodeNumber);
 }
 
-window.onload = setup;
+window.onload = setup();
